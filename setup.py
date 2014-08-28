8cf77e9eb20b50ff13f5b6c85c57eeaa86488901
Aaron Bandt <Aaron.Bandt@citygridmedia.com>
2014-08-28 09:34:21 -0700

    renaming twonicorn to twonicorn-web-lib

--- setup.py
+++ setup.py
@@ -13,6 +13,7 @@ requires = [
     'pyramid_chameleon',
     'pyramid_debugtoolbar',
     'waitress',
+    'twonicorn-web-lib',
     ]
 
 setup(name='twonicorn-web',
--- twonicornweb/views.py
+++ twonicornweb/views.py
@@ -1,5 +1,5 @@
 from pyramid.view import view_config
-import twonicorn
+import twonicorn-web-lib
 
 
 @view_config(route_name='home', renderer='templates/home.pt')
