7d2533e924da9c58241274449b6204e0f4092a7e
Aaron Bandt <Aaron.Bandt@citygridmedia.com>
2014-09-24 09:42:58 -0700

    updating lib version again

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ requires = [
     'pyramid_ldap==0.2',
     'python-ldap==2.4.16',
     'ldappool==1.0',
-    'TwonicornWebLib==1.2',
+    'TwonicornWebLib==1.3',
     ]
 
 setup(name='TwonicornWeb',
