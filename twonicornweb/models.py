42ce799a280e19d02097e0e60bfe83c5a4b921fa
Wil Heitritter <Wil.Heitritter@citygridmedia.com>
2014-10-07 13:58:47 -0700

    Adding relationships and foreign keys...untested.

--- twonicornweb/models.py
+++ twonicornweb/models.py
@@ -37,16 +37,20 @@ class Applications(Base):
 class Deploys(Base):
     __tablename__ = 'deploys'
     deploy_id        = Column(Integer, primary_key=True, nullable=False)
-    application_id   = Column(Integer, nullable=False)
-    artifact_type_id = Column(Integer, nullable=False)
+    application_id   = Column(Integer, nullable=False,
+                              ForeignKey('applications.application_id'))
+    artifact_type_id = Column(Integer, nullable=False,
+                              ForeignKey('artifacts.artifact_id'))
     deploy_path      = Column(Text, nullable=False)
     created          = Column(TIMESTAMP, nullable=False)
+    application      = relationship("Applications", backref=backref('deploys'))
 
 
 class Artifacts(Base):
     __tablename__ = 'artifacts'
     artifact_id = Column(Integer, primary_key=True, nullable=False)
-    repo_id     = Column(Integer, nullable=False)
+    repo_id     = Column(Integer, nullable=False,
+                         ForeignKey('repos.repo_id'))
     location    = Column(Text, nullable=False)
     revision    = Column(Text, nullable=False)
     branch      = Column(Text)
@@ -57,46 +61,59 @@ class Artifacts(Base):
 class ArtifactAssignments(Base):
     __tablename__     = 'artifact_assignments'
     artifact_assignment_id = Column(Integer, primary_key=True, nullable=False)
-    deploy_id              = Column(Integer, nullable=False)
-    env_id                 = Column(Integer, nullable=False)
-    lifecycle_id           = Column(Integer, nullable=False)
-    artifact_id            = Column(Integer, nullable=False)
+    deploy_id              = Column(Integer, nullable=False,
+                                    ForeignKey('deploys.deploy_id'))
+    env_id                 = Column(Integer, nullable=False,
+                                    ForeignKey('envs.env_id'))
+    lifecycle_id           = Column(Integer, nullable=False,
+                                    ForeignKey('lifecycles.lifecycle_id'))
+    artifact_id            = Column(Integer, nullable=False,
+                                    ForeignKey('artifacts.artifact_id'))
     user                   = Column(Text, nullable=False)
     created                = Column(TIMESTAMP, nullable=False)
+    deploy                 = relationship("Deploys", backref=backref('artifact_assignments'))
+    artifact               = relationship("Artifacts", backref=backref('artifact_assignments'))
 
 
 class ArtifactNotes(Base):
     __tablename__ = 'artifact_notes'
     artifact_note_id = Column(Integer, primary_key=True, nullable=False)
-    artifact_id      = Column(Integer, nullable=False)
+    artifact_id      = Column(Integer, nullable=False,
+                              ForeignKey('artifacts.artifact_id'))
     user             = Column(Text, nullable=False)
     note             = Column(Text, nullable=False)
     created          = Column(TIMESTAMP, nullable=False)
+    artifact         = relationship("Artifacts", backref=backref('notes'))
 
 
 class Lifecycles(Base):
     __tablename__ = 'lifecycles'
-    lifecycle_id = Column(Integer, primary_key=True, nullable=False)
-    name         = Column(Text, nullable=False)
+    lifecycle_id         = Column(Integer, primary_key=True, nullable=False)
+    name                 = Column(Text, nullable=False)
+    artifact_assignments = relationship("ArtifactAssignments", backref=backref('lifecycle'))
 
 
 class Envs(Base):
     __tablename__ = 'envs'
     env_id = Column(Integer, primary_key=True, nullable=False)
     name   = Column(Text, nullable=False)
+    artifact_assignments = relationship("ArtifactAssignments", backref=backref('env'))
 
 
 class Repos(Base):
     __tablename__ = 'repos'
     repo_id      = Column(Integer, primary_key=True, nullable=False)
-    repo_type_id = Column(Integer, nullable=False)
+    repo_type_id = Column(Integer, nullable=False,
+                          ForeignKey('repo_types.repo_type_id'))
     name         = Column(Text, nullable=False)
+    artifacts    = relationship("Artifacts", backref=backref('repos'))
 
 
 class RepoTypes(Base):
     __tablename__ = 'repo_types'
     repo_type_id = Column(Integer, primary_key=True, nullable=False)
     name         = Column(Text, nullable=False)
+    repos        = relationship("Repos", backref=backref('type'))
 
 
 class ArtifactTypes(Base):
@@ -108,6 +125,9 @@ class ArtifactTypes(Base):
 class RepoUrls(Base):
     __tablename__ = 'repo_urls'
     repo_url_id = Column(Integer, primary_key=True, nullable=False)
-    repo_id     = Column(Integer, nullable=False)
+    repo_id     = Column(Integer, nullable=False,
+                         ForeignKey('repos.repo_id'))
     ct_loc      = Column(Text, nullable=False)
     url         = Column(Text, nullable=False)
+    repo        = relationship("Repos", backref=backref('url'))
+
