011f0d441764250bd14f99abb0087dc8be76b2ed
Aaron Bandt <Aaron.Bandt@citygridmedia.com>
2015-03-16 09:58:30 -0700

    moving to an external fork of pyramid_ldap

--- setup.py
+++ setup.py
@@ -40,6 +40,11 @@ requires = [
     'passlib==1.6.2',
     ]
 
+dependency_links = [
+    'http://cdn.mysql.com/Downloads/Connector-Python/mysql-connector-python-1.2.3.zip#md5=6d42998cfec6e85b902d4ffa5a35ce86'
+    'https://github.com/CityGrid/pyramid_ldap'
+    ]
+
 setup(name='TwonicornWeb',
       version='2.8.5',
       description='Twonicorn web api/ui',
@@ -59,7 +64,7 @@ setup(name='TwonicornWeb',
       include_package_data=True,
       zip_safe=False,
       install_requires=requires,
-      dependency_links = ['http://cdn.mysql.com/Downloads/Connector-Python/mysql-connector-python-1.2.3.zip#md5=6d42998cfec6e85b902d4ffa5a35ce86'],
+      dependency_links = dependency_links,
       tests_require=requires,
       test_suite="TwonicornWeb",
       entry_points="""\
