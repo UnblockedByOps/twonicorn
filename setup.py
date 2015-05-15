d5da8181e5d2917003cd4b6548539fc49568b985
Aaron Bandt <Aaron.Bandt@citygridmedia.com>
2015-05-15 09:43:21 -0700

    Bumping revision for next dev cycle

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ dependency_links = [
     ]
 
 setup(name='TwonicornWeb',
-      version='2.8.6',
+      version='2.8.7',
       description='Twonicorn web api/ui',
       long_description=README + '\n\n' + CHANGELOG,
       classifiers=[
