58193e2ae542f6352818918e9cda32ec615c701a
Aaron Bandt <Aaron.Bandt@citygridmedia.com>
2015-05-20 10:16:01 -0700

    Updated/cleaned up some styles and a more unified look & feel.

--- twonicornweb/static/row_expansion_app.js
+++ twonicornweb/static/row_expansion_app.js
@@ -17,13 +17,12 @@ $(document).ready(function(){
    var i=1;
 
    $("#add_row").click(function(){
-      $('#addr'+i).html('</td> <td> <span for="artifact_type'+i+'">Artifact Type:</span><br> <select id="artifact_type'+i+'" name="artifact_type" placeholder="Select Something" required> </select> <span class="form_hint">Artifact type</span> </td> <td> <span for="deploy_path'+i+'">Deploy Path:</span><br> <input type="text" id="deploy_path'+i+'" name="deploy_path" placeholder="/app/tomcat/conf" required/> <span class="form_hint">Where this deploy gets installed</span> </td> <td> <span for="package_name'+i+'">Package Name:</span><br> <input type="text" id="package_name'+i+'" name="package_name" placeholder="MyPythonPackage"/> <span class="form_hint">Required for deploys that are deployed using a native package management tool (python (pip), rpm (yum), etc.)</span> </td>');
+      $('#addr'+i).html('</td> <td> <span for="artifact_type-'+i+'">Artifact Type:</span><br> <select id="artifact_type-'+i+'" name="artifact_type" placeholder="Select Something" required> </select> <span class="form_hint">Artifact type</span> </td> <td> <span for="deploy_path'+i+'">Deploy Path:</span><br> <input type="text" id="deploy_path'+i+'" name="deploy_path" placeholder="/app/tomcat/conf" required/> <span class="form_hint">Where this deploy gets installed</span> </td> <td id="pkg_td'+i+'" class="hidden"> <span for="package_name-'+i+'">Package Name:</span><br> <input type="text" id="package_name'+i+'" name="package_name" placeholder="MyPythonPackage"/> <span class="form_hint">Required for deploys that are deployed using a native package management tool (python (pip), rpm (yum), etc.)</span> </td>');
 
-//      $('#artifact_type0:last option').clone().appendTo('#artifact_type'+i+'');
-      var choices = $('#artifact_type0:last option').clone()
+      var choices = $('#artifact_type-0:last option').clone()
       choices.removeAttr('selected');
       choices.first().prop( 'selected', true );
-      choices.appendTo('#artifact_type'+i+'');
+      choices.appendTo('#artifact_type-'+i+'');
 
       $('#tab_deploy').append('<tr id="addr'+(i+1)+'"></tr>');
       i++;
